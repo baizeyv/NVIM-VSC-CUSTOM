a3fc2d7198dd612bd70e65b38b8d5459da331423
baizeyv <76252320+baizeyv@users.noreply.github.com>
2026-09-18 13:30:22 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,17 +1,15 @@
--- 保存原来的 vim.notify
-local original_notify = vim.notify
-
--- 重写 vim.notify 函数
+-- set custom notify
+local native_notify = vim.notify
 vim.notify = function(msg, log_level, opts)
-    -- 使用 VS Code 通知
-    local vscode_notify = require("vscode").notify
-
-    -- 通过 VS Code 进行通知
-    vscode_notify(msg)
-
-    -- 调用原来的 vim.notify
-    original_notify(msg, log_level, opts)
+    -- use vsc notify
+    if (vim.g.vscode) then
+        local vsc_notify = require("vscode").notify
+        vsc_notify(msg)
+    else
+        native_notify(msg, log_level, opts)
+    end
 end
 
-vim.keymap.set("","<Space>","<Nop>")
-require("config.plugin-loader")
+require('config').setup()
+require('config.plugin-loader').load()
+
