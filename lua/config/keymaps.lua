11c67904893fab5901688965fbb5566bea7b548b
baizeyv <76252320+baizeyv@users.noreply.github.com>
2026-09-18 13:31:49 +0800

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,16 +6,23 @@ local map = function(modes, lhs, rhs, opts)
     end
 end
 
+local unmap = function(modes, key)
+    if #modes > 0 then vim.keymap.del(modes, key) end
+end
+
 local keys = require("config.keys")
 
 local map_specific = function(name)
     for lhs, cfg in pairs(keys[name]) do
-        if lhs ~= cfg[1] then
-            map(cfg[2], lhs, cfg[1], cfg[3])
-        end
+        if lhs ~= cfg[1] then map(cfg[2], lhs, cfg[1], cfg[3]) end
     end
 end
 
+local unmap_specific = function(name)
+    for k, modes in pairs(keys[name]) do unmap(modes, k) end
+end
+
+map_specific("Vsc")
 map_specific("Generic")
 map_specific("Other1")
 map_specific("Other2")
@@ -26,9 +33,12 @@ map_specific("CMP")
 map_specific("Search")
 map_specific("Fold")
 map_specific("Bookmark")
+map_specific("CommentLens")
+map_specific("TurboFileHeader")
 map_specific("ZenMode")
 
-vim.api.nvim_exec_autocmds("User", {
-    pattern = "VSC_NVM_KEYMAP",
-    modeline = false
-})
+unmap_specific("Rubbish")
+
+local fast_move = require("config.fast-cursor-move")
+fast_move.setup()
+
